13fcd43a8720f37b7ea1f2eab4454e4171d65894
Dana A. Turysbekova <73177703+dqnondee@users.noreply.github.com>
2023-03-11 10:00:18 +0600

    Add files via upload

---
+++ lab5(1)/src/app/categories.ts
@@ -0,0 +1,24 @@
+export interface Category {
+  id: number;
+  name: string;
+  
+}
+
+export const categories = [
+  {
+    id: 1,
+    name: 'Sport'
+  },
+    {
+      id:2,
+      name:'Phone'
+    },
+    {
+      id:3,
+      name:'usb'
+    },
+    {
+      id:4,
+      name:'beauty'
+    }
+];
---
+++ lab5(1)/src/app/products.ts
@@ -0,0 +1,78 @@
+export interface Product {
+  id: number;
+  name: string;
+  price: number;
+  description: string;
+  sshow: boolean;
+}
+
+export const products = [
+  {
+    id: 1,
+    name: 'Смартфон Xiaomi Redmi Note 10 Pro 8 ГБ/256 ГБ серый',
+    price: 128350,
+    rating: '4.68/5',
+    image:
+      'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/h59/hcf/64487157956638/xiaomi-redmi-note-10-pro-8-gb-128-gb-seryi-107221005-1.jpg',
+    description:
+      'технология NFC: Да,цвет: серый,тип экрана: цветной AMOLED, сенсорный,диагональ: 6.67 дюйм',
+    sshow: false,
+    link: 'https://kaspi.kz/shop/p/xiaomi-redmi-note-10-pro-8-gb-256-gb-seryi-107221005/?c=750000000&utm_source=google&utm_medium=cpc&utm_campaign=shop_google_search_kaspi_brand_desktop&gclid=CjwKCAiAjPyfBhBMEiwAB2CCIrPQtX3ACRkZwfPt5DL7GuPhIp7NTcNiWFYHoxYUXYsrQvpn7EYx8xoC-PwQAvD_BwE#!/item',
+  },
+
+  {
+    id: 2,
+    name: 'Смартфон Apple iPhone 11 128Gb Slim Box черный',
+    price: 293712,
+    rating: '4.33/5',
+    image:
+      'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/h0a/h2e/31489167622174/apple-iphone-11-128gb-slim-box-cernyj-100692388-1-Container.jpg',
+    description:
+      'Функциональный и стильный смартфон Apple iPhone 11 в металлическом корпусе способен обеспечить не только повседневное общение и развлечения, но и продуктивную работу.',
+    sshow: false,
+    link: 'https://kaspi.kz/shop/p/apple-iphone-11-128gb-slim-box-chernyi-100692388/?c=750000000&utm_source=google&utm_medium=cpc&utm_campaign=shop_google_search_kaspi_brand_desktop&gclid=CjwKCAiAjPyfBhBMEiwAB2CCIrPQtX3ACRkZwfPt5DL7GuPhIp7NTcNiWFYHoxYUXYsrQvpn7EYx8xoC-PwQAvD_BwE#!/item',
+  },
+  {
+    id: 3,
+    name: 'Смартфон Xiaomi Redmi Note 10 Pro 8 ГБ/256 ГБ серый',
+    price: 128350,
+    rating: '4.88/5',
+    image:
+      'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/h59/hcf/64487157956638/xiaomi-redmi-note-10-pro-8-gb-128-gb-seryi-107221005-1.jpg',
+    description:
+      'Функциональный и стильный смартфон обеспечить не только повседневное общение и развлечения, но и продуктивную работу.',
+    sshow: false,
+    link: 'https://kaspi.kz/shop/p/xiaomi-redmi-note-10-pro-8-gb-256-gb-seryi-107221005/?c=750000000&utm_source=google&utm_medium=cpc&utm_campaign=shop_google_search_kaspi_brand_desktop&gclid=CjwKCAiAjPyfBhBMEiwAB2CCIrPQtX3ACRkZwfPt5DL7GuPhIp7NTcNiWFYHoxYUXYsrQvpn7EYx8xoC-PwQAvD_BwE#!/item',
+  },
+
+  {
+    id: 4,
+    name: 'Смартфон Samsung Galaxy A13 4 ГБ/128 ГБ черный',
+    price: 88680,
+    rating: '4.7/5',
+    image:
+      'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/hf2/hbe/49613711933470/samsung-galaxy-a13-4-gb-128-gb-chernyi-104253279-1.jpg',
+    description:
+      'Благодаря 6.6-дюймовому экрану Infinity-V на смартфоне Galaxy A13 вы сможете увидеть все, что было скрыто от вашего взгляда. Наслаждайтесь живым четким изображением с технологией FHD+. ',
+    sshow: false,
+    link: 'https://kaspi.kz/shop/p/samsung-galaxy-a13-4-gb-128-gb-chernyi-104253279/?c=750000000&utm_source=google&utm_medium=cpc&utm_campaign=shop_google_search_kaspi_brand_desktop&gclid=CjwKCAiAjPyfBhBMEiwAB2CCIrPQtX3ACRkZwfPt5DL7GuPhIp7NTcNiWFYHoxYUXYsrQvpn7EYx8xoC-PwQAvD_BwE#!/item',
+  },
+  {
+    id: 5,
+    name: 'Смартфон Xiaomi Redmi Note 11 Pro 8 ГБ/128 ГБ серый',
+    price: 138990,
+    rating: '4.75/5',
+    image:
+      'https://resources.cdn-kaspi.kz/shop/medias/sys_master/images/images/hff/h0f/51166990336030/xiaomi-redmi-note-11-pro-8-gb-128-gb-seryi-103971386-1.jpg',
+    description:
+      'Сверхвысокая частота обновления. Высокая частота обновления 120 Гц обеспечивает бесперебойную работу и высокую детализацию изображения при просмотре веб-страниц.',
+    sshow: false,
+    link: 'https://kaspi.kz/shop/p/xiaomi-redmi-note-11-pro-8-gb-128-gb-seryi-103971386/?c=750000000&utm_source=google&utm_medium=cpc&utm_campaign=shop_google_search_kaspi_brand_desktop&gclid=CjwKCAiAjPyfBhBMEiwAB2CCIrPQtX3ACRkZwfPt5DL7GuPhIp7NTcNiWFYHoxYUXYsrQvpn7EYx8xoC-PwQAvD_BwE#!/item',
+  },
+];
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
