087b54d286da5376fcec3b742c876d72f74da8bb
Dana A. Turysbekova <73177703+dqnondee@users.noreply.github.com>
2023-03-26 22:07:31 +0600

    task2

---
+++ lab7/task 2/Task2/tasks2/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\ati_karash\Documents\WebDevelopment\Lab7\Task2\task2\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
