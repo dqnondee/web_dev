6cf16e73d8b14ea6f1ebe3af584a2597186d1d49
Dana A. Turysbekova <73177703+dqnondee@users.noreply.github.com>
2023-04-12 22:22:28 +0600

    Delete lab9/venv directory

--- lab9/venv/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!C:\Users\User_0\Desktop\WebDev\Laboratory Works\Lab_9\venv\Scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
